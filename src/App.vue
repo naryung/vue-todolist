c861c222ae54d6307038b764e183a906f4c152ad
naryung <naryung95@naver.com>
2019-08-07 15:58:21 +0900

    vue todo list web

---
+++ src/App.vue
@@ -0,0 +1,71 @@
+<template>
+  <div id="app">
+    <TodoHeader></TodoHeader>
+    <TodoInput v-on:addTodo="addTodo"></TodoInput>
+    <TodoList v-bind:propsdata="todoItems" v-on:removeTodo="removeTodo"></TodoList>
+    <TodoFooter v-on:removeAll="clearAll"></TodoFooter>
+  </div>
+</template>
+
+<script>
+import TodoHeader from './components/TodoHeader.vue'
+import TodoInput from './components/TodoInput.vue'
+import TodoList from './components/TodoList.vue'
+import TodoFooter from './components/TodoFooter.vue'
+
+export default {
+  data() {
+    return {
+      todoItems: []
+    }
+  },
+  created() {
+    if(localStorage.length > 0){
+        for(var i=0; i<localStorage.length; i++){
+          if(localStorage.key(i) === 'loglevel:webpack-dev-server') continue;
+          this.todoItems.push(localStorage.key(i));
+        }
+    }
+  },
+  methods: {
+    // 로컬 스토리지에 데이터를 추가하는 로직
+    addTodo(todoItem){
+      localStorage.setItem(todoItem, todoItem);
+      this.todoItems.push(todoItem);
+    },
+    // 로컬 스토리지에 있는 특정 데이터 삭제하는 로직
+    removeTodo(todoItem, index) {
+      localStorage.removeItem(todoItem);
+      this.todoItems.splice(index, 1);
+    },
+    // 로컬 스토리지에 있는 모든 데이터 삭제하는 로직
+    clearAll() {
+      localStorage.clear();
+      this.todoItems = [];
+    }
+  },
+  components:{
+    'TodoHeader' : TodoHeader,
+    'TodoInput' : TodoInput,
+    'TodoList' : TodoList,
+    'TodoFooter' : TodoFooter
+  }
+}
+</script>
+
+<style>
+body{
+    text-align: center;
+    background-color: #F6F6F8;
+}
+input {
+    border-style: groove;
+    width: 200px;
+}
+button {
+  border-style: groove;
+}
+.shadow {
+  box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03)
+}
+</style>
---
+++ src/components/TodoFooter.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="clearAllContainer">
+        <span class="clearAllBtn" @click="clearTodo">Clear All</span>
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        // removeAll 이벤트를 발생시켜 App 컴포넌트로 전달하는 로직
+        clearTodo() {
+            this.$emit('removeAll');
+        }
+    }
+}
+</script>
+
+<style scoped>
+.clearAllContainer {
+    width: 8.5em;
+    height: 50px;
+    line-height: 50px;
+    background-color: white;
+    border-radius: 5px;
+    margin: 0 auto;    
+}
+.clearAllBtn {
+    color: #e20303;
+    display: black;
+}
+</style>
